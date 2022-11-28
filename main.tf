ca7a6342075c17108f5d27ed5f6c3abd6b453659
Krzysztof Niepokojczycki <krzysztofn@spacelift.io>
2022-11-28 16:47:22 +0100

    something something

--- main.tf
+++ main.tf
@@ -4,8 +4,29 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.6.0"
     }
+    random = { source = "hashicorp/random" }
   }
 }
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_vpc" "this" {
+  default = true
+}
+
+data "aws_security_group" "this" {
+  name   = "default"
+  vpc_id = data.aws_vpc.this.id
+}
+
+data "aws_subnet_ids" "this" {
+  vpc_id = data.aws_vpc.this.id
+}
+
+
+resource "random_pet" "this" {}
 
 module "my_workerpool" {
   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=e954914020a5e1a561038cba27bb9d06438deba6"
@@ -16,8 +37,8 @@ module "my_workerpool" {
   EOT
 
   max_size          = 2
-  ami_id            = var.worker_pool_ami_id
-  worker_pool_id    = var.worker_pool_id
-  security_groups   = var.worker_pool_security_groups
-  vpc_subnets       = var.worker_pool_subnets
+  ami_id            = "ami-0ecc1e917fcb34066"
+  worker_pool_id    = random_pet.this.id
+  security_groups = [data.aws_security_group.this.id]
+  vpc_subnets     = data.aws_subnet_ids.this.ids
 }
